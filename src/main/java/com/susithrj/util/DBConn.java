d3082a6fabec10e8ac52ad833d22f2530f060863
susithrj <susithlion@gmail.com>
2021-07-29 08:56:09 +0530

    fix jdbc server timezone issue

--- src/main/java/com/susithrj/util/DBConn.java
+++ src/main/java/com/susithrj/util/DBConn.java
@@ -1,52 +1,53 @@
-//package com.susithrj.util;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//
-//import static java.lang.System.err;
-//
-//public class DBConn {
-//    private  static  DBConn dbConn = new DBConn();
-//    private DBConn(){
-//
-//    }
-//    public static DBConn getInstance(){
-//        if (dbConn == null)
-//        {
-//            dbConn = new DBConn();
-//        }
-//        return dbConn;
-//    }
-//
-//    public void makeJDBCConn(){
-//        try {
-//            Class.forName("com.mysql.jdbc.Driver");
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//            err.println("jdbc class not found");
-//        }
-//        Connection conn = null;
-//        try {
-//            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/smartguru_db", "root", "");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        if (conn != null) {
-//            System.out.println("Connection Successful! Enjoy. Now it's time to pull data");
-//        } else {
-//            System.out.println("Failed to make connection!");
-//        }
-//
-//        //todo queriying database in another class
-//
-//
-//        try {
-//            conn.close();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            System.out.println("connection successfully closed");
-//        }
-//
-//    }
-//}
+package com.susithrj.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static java.lang.System.err;
+
+public class DBConn {
+    private  static  DBConn dbConn = new DBConn();
+    private DBConn(){
+
+    }
+    public static DBConn getInstance(){
+        if (dbConn == null)
+        {
+            dbConn = new DBConn();
+        }
+        return dbConn;
+    }
+
+    public static void makeJDBCConn(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            err.println("jdbc class not found");
+        }
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/smartguru_db?serverTimezone=UTC", "root", "");
+          //  jdbc:mysql://localhost:3306/crudapi?serverTimezone=UTC
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (conn != null) {
+            System.out.println("Connection Successful! Enjoy. Now it's time to pull data");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+
+        //todo queriying database in another class
+
+
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("connection successfully closed");
+        }
+
+    }
+}
